148d75f51a170ca71779de5b07a88bddc0654ddf
Dan Savidge <dsavidge02@gmail.com>
2024-07-11 12:19:35 -0400

    basic react layout

---
+++ client/src/components/Featured.jsx
@@ -0,0 +1,12 @@
+import { Container, Form, Button } from "react-bootstrap";
+
+const Featured = (props) => {
+  return (
+    <Container className="home-page-container">
+      <Container className="featured-container"></Container>
+      <Container className="shop-container"></Container>
+    </Container>
+  );
+};
+
+export default Featured;
